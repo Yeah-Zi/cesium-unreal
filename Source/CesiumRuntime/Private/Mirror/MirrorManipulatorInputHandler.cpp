98a754c53fc764fc098af652cfa8df15eed98db4
Leaf <1216657464@qq.com>
2024-05-24 16:35:42 +0800

    feat: 新增拖拽操作类

--- Source/CesiumRuntime/Private/Mirror/MirrorManipulatorInputHandler.cpp
+++ Source/CesiumRuntime/Private/Mirror/MirrorManipulatorInputHandler.cpp
@@ -2,4 +2,86 @@
 
 
 #include "Mirror/MirrorManipulatorInputHandler.h"
+#include "Mirror/MirrorCoordinatesBPFuncLibrary.h"
+FVector2D UMirrorManipulatorInputHandler::GetControllerMousePosition() {
+  APawn* Owner = Cast<APawn>(GetOwner());
+  APlayerController* Controller =
+      Cast<APlayerController>(Owner->GetController());
 
+  FVector2D MouseScreenPosition;
+  Controller->GetMousePosition(MouseScreenPosition.X, MouseScreenPosition.Y);
+  return MouseScreenPosition;
+}
+
+void UMirrorManipulatorInputHandler::PressedAction(FKey key) {
+  FVector2D MouseScreenPosition = GetControllerMousePosition();
+  if (key == EKeys::LeftMouseButton) {
+    LeftMouseButtonPressed(MouseScreenPosition);
+  }
+}
+
+void UMirrorManipulatorInputHandler::ReleasedAction(FKey key) {
+  FVector2D MouseScreenPosition = GetControllerMousePosition();
+  if (key == EKeys::LeftMouseButton) {
+    LeftMouseButtonReleased(MouseScreenPosition);
+  }
+}
+
+void UMirrorManipulatorInputHandler::LeftMouseButtonPressed(
+    FVector2D MouseScreenPosition) {
+
+}
+
+void UMirrorManipulatorInputHandler::LeftMouseButtonReleased(
+    FVector2D MouseScreenPosition) {
+
+}
+
+void UMirrorManipulatorInputHandler::MouseMoveAxis(float value) {
+  FVector2D MouseScreenPosition = GetControllerMousePosition();
+  if (value > 0) {
+    OnMouseMove(MouseScreenPosition);
+  }
+}
+
+void UMirrorManipulatorInputHandler::OnMouseMove(
+    const FVector2D& MouseScreenPosition) {}
+
+
+
+void UMirrorDragInputHandler::LeftMouseButtonPressed(
+    FVector2D MouseScreenPosition) {
+  IsLeftMouseButtonPressed = true;
+  DraggingStartEarthPositionInCameraCoordinate;
+
+  APawn* Owner = Cast<APawn>(GetOwner());
+  APlayerController* Controller =
+      Cast<APlayerController>(Owner->GetController());
+
+  FVector UnrealLocation, UnrealDirection;
+  Controller->DeprojectMousePositionToWorld(UnrealLocation, UnrealDirection);
+
+
+  FHitResult HitResultInUnreal =
+      UMirrorCoordinatesBPFuncLibrary::LineTraceRealEarthInUnreal(
+          UnrealLocation,
+          UnrealLocation + 99999999999 * UnrealDirection);
+
+  FTransform CameraInUnrealTransform;
+  FVector Location;
+  FRotator Rotation;
+  Controller->GetPlayerViewPoint(Location, Rotation);
+  CameraInUnrealTransform.SetLocation(Location);
+  CameraInUnrealTransform.SetRotation(FQuat(Rotation));
+
+  //if (UMirrorCoordinatesBPFuncLibrary::LineTraceRealEarthInECEF()) {
+  //}
+}
+
+void UMirrorDragInputHandler::LeftMouseButtonReleased(
+    FVector2D MouseScreenPosition) {
+  IsLeftMouseButtonPressed =  false;
+}
+
+void UMirrorDragInputHandler::OnMouseMove(
+    const FVector2D& MouseScreenPosition) {}
--- Source/CesiumRuntime/Public/Mirror/MirrorManipulatorInputHandler.h
+++ Source/CesiumRuntime/Public/Mirror/MirrorManipulatorInputHandler.h
@@ -7,11 +7,43 @@
 #include "MirrorManipulatorInputHandler.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class CESIUMRUNTIME_API UMirrorManipulatorInputHandler : public UMirrorInputRegisterComponent
-{
-	GENERATED_BODY()
-	
+class CESIUMRUNTIME_API UMirrorManipulatorInputHandler
+    : public UMirrorInputRegisterComponent {
+  GENERATED_BODY()
+
+public:
+  FVector2D GetControllerMousePosition();
+
+  virtual void PressedAction(struct FKey key);
+  virtual void ReleasedAction(struct FKey key);
+
+  virtual void LeftMouseButtonPressed(FVector2D MouseScreenPosition);
+  virtual void LeftMouseButtonReleased(FVector2D MouseScreenPosition);
+
+  virtual void MouseMoveAxis(float value);
+
+  virtual void OnMouseMove(const FVector2D& MouseScreenPosition);
+};
+
+UCLASS()
+class CESIUMRUNTIME_API UMirrorDragInputHandler
+    : public UMirrorManipulatorInputHandler {
+  GENERATED_BODY()
+public:
+  bool IsLeftMouseButtonPressed = false;
+
+  double DraggingInterval = 0.2;
+
+  double DraggingStartVirtualEarthRadius;
+  FVector DraggingStartEarthPositionInCameraCoordinate;
+  FVector DraggingNowEarthPositionInCameraCoordinate;
+
+  virtual void LeftMouseButtonPressed(FVector2D MouseScreenPosition);
+  virtual void LeftMouseButtonReleased(FVector2D MouseScreenPosition);
+
+  virtual void OnMouseMove(const FVector2D& MouseScreenPosition);
+
 };
